11a0a258103c0dda6b4256c6fbda21b6fec0813e
user <bakr.frag@gmail.com>
2019-06-11 16:53:08 +0200

    Try To Customize Catogrey planning

--- pim/category/models.py
+++ pim/category/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name;
 class Product(models.Model):
-    under_category=models.ManyToManyField(Category,);
+    under_category=models.ManyToManyField(Category);
     name=models.CharField(max_length=256);
     quantity=models.IntegerField();
     price=models.CharField(max_length=256);
