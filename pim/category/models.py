ed4a9bb7cbc16c0ab2eff184fb70842de63f82f6
user <>
2019-06-11 13:24:36 +0200

    solve problem name conflic and migrate

--- pim/category/models.py
+++ pim/category/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     root=models.ForeignKey('self',on_delete=models.CASCADE,blank=True,null=True,);
-    category=models.CharField(max_length=256,);
+    category_name=models.CharField(max_length=256,);
     def __str__(self):
         return self.category;
 class Product(models.Model):
-    under_category=models.ManyToManyField(Category,realted_name="sub_category");
+    under_category=models.ManyToManyField(Category,);
     name=models.CharField(max_length=256);
     quantity=models.IntegerField();
     price=models.CharField(max_length=256);
