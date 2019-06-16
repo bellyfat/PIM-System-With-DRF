5a6c9580468ff332b1538a24a8a0fe8ff0dbbde7
user <>
2019-06-16 12:13:37 +0200

    add objects after add custom model manager

--- pim/category/models.py
+++ pim/category/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class NumberChilds(models.Manager):
+    def get_queryset(self):
+        return self.childs.count();
 class Category(models.Model):
+    objects = models.Manager()
     root=models.ForeignKey('self',on_delete=models.CASCADE,blank=True,null=True,related_name="childs");
     category_name=models.CharField(max_length=256,);
+    number_of_childs=NumberChilds();
     def __str__(self):
         return self.category_name;
 class Product(models.Model):
